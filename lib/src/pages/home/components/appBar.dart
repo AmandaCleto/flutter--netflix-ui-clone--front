c4772898ef892cbd1feb93719110e3ffcc05ceb1
AmandaCleto <amanda.clet@gmail.com>
2021-09-23 23:16:35 -0300

    creating animation of scroll to appbar

--- lib/src/pages/home/components/appBar.dart
+++ lib/src/pages/home/components/appBar.dart
@@ -2,57 +2,49 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-class CustomAppBar extends StatelessWidget {
+class CustomAppBar extends StatefulWidget {
   final double scrollOffset;
+  final bool hideTopAppBar;
 
   const CustomAppBar({
-    this.scrollOffset = 0.0,
-  });
+    Key? key,
+    required this.scrollOffset,
+    required this.hideTopAppBar,
+  }) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return Container(
-      color:
-          Colors.black.withOpacity((scrollOffset / 350).clamp(0, 1).toDouble()),
-      child: _CustomAppBarMobile(),
-    );
-  }
+  _CustomAppBarState createState() => _CustomAppBarState();
 }
 
-class _CustomAppBarMobile extends StatefulWidget {
-  @override
-  __CustomAppBarMobileState createState() => __CustomAppBarMobileState();
-}
-
-class __CustomAppBarMobileState extends State<_CustomAppBarMobile> {
+class _CustomAppBarState extends State<CustomAppBar> {
   late ScrollController _scrollViewController;
-  bool _showAppbar = true;
+
   bool isScrollingDown = false;
 
-  @override
-  void initState() {
-    super.initState();
-    _scrollViewController = new ScrollController();
-    _scrollViewController.addListener(() {
-      if (_scrollViewController.position.userScrollDirection ==
-          ScrollDirection.reverse) {
-        if (!isScrollingDown) {
-          isScrollingDown = true;
-          _showAppbar = false;
-          setState(() {});
-        }
-      }
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   _scrollViewController = new ScrollController();
+  //   _scrollViewController.addListener(() {
+  //     if (_scrollViewController.position.userScrollDirection ==
+  //         ScrollDirection.reverse) {
+  //       if (!isScrollingDown) {
+  //         isScrollingDown = true;
+  //         widget.hideTopAppBar = false;
+  //         setState(() {});
+  //       }
+  //     }
 
-      if (_scrollViewController.position.userScrollDirection ==
-          ScrollDirection.forward) {
-        if (isScrollingDown) {
-          isScrollingDown = false;
-          _showAppbar = true;
-          setState(() {});
-        }
-      }
-    });
-  }
+  //     if (_scrollViewController.position.userScrollDirection ==
+  //         ScrollDirection.forward) {
+  //       if (isScrollingDown) {
+  //         isScrollingDown = false;
+  //         widget.hideTopAppBar = true;
+  //         setState(() {});
+  //       }
+  //     }
+  //   });
+  // }
 
   @override
   void dispose() {
@@ -63,57 +55,102 @@ class __CustomAppBarMobileState extends State<_CustomAppBarMobile> {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Flex(
-        direction: Axis.vertical,
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          Expanded(
-            child: Column(
-              children: <Widget>[
-                AnimatedContainer(
-                  height: _showAppbar ? 100.0 : 0.0,
-                  width: MediaQuery.of(context).size.width,
-                  duration: Duration(milliseconds: 200),
-                  child: AppBar(
-                    backgroundColor: Colors.transparent,
-                    elevation: 0,
-                    flexibleSpace: Column(
-                      children: [
-                        Container(
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
+    return Container(
+      color: Colors.black
+          .withOpacity((widget.scrollOffset / 350).clamp(0, 1).toDouble()),
+      child: SafeArea(
+        child: Flex(
+          direction: Axis.vertical,
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Expanded(
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    width: MediaQuery.of(context).size.width,
+                    child: AppBar(
+                      backgroundColor: Colors.transparent,
+                      elevation: 0,
+                      flexibleSpace: Column(
+                        children: [
+                          AnimatedContainer(
+                            height: widget.hideTopAppBar ? 60.0 : 10.0,
+                            duration: Duration(milliseconds: 0),
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Row(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Image.asset(
+                                    "assets/netflix-logo.png",
+                                    width: 20,
+                                    fit: BoxFit.cover,
+                                  ),
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.center,
+                                    children: [
+                                      SvgPicture.asset(
+                                        'assets/icons/search.svg',
+                                        height: 20,
+                                        width: 20,
+                                        allowDrawingOutsideViewBox: true,
+                                      ),
+                                      SizedBox(
+                                        width: 22,
+                                      ),
+                                      ClipRRect(
+                                        borderRadius: BorderRadius.all(
+                                          Radius.circular(2),
+                                        ),
+                                        child: Image.asset(
+                                          "assets/user-hero.png",
+                                          height: 22,
+                                          width: 22,
+                                          fit: BoxFit.cover,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Container(
                             child: Row(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
                               children: [
-                                Image.asset(
-                                  "assets/netflix-logo.png",
-                                  width: 20,
-                                  fit: BoxFit.cover,
+                                Text(
+                                  'Séries',
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                                Text(
+                                  'Filmes',
+                                  style: TextStyle(color: Colors.white),
                                 ),
                                 Row(
-                                  mainAxisAlignment: MainAxisAlignment.center,
                                   crossAxisAlignment: CrossAxisAlignment.center,
                                   children: [
-                                    SvgPicture.asset(
-                                      'assets/icons/search.svg',
-                                      height: 20,
-                                      width: 20,
-                                      allowDrawingOutsideViewBox: true,
+                                    Text(
+                                      'Categorias',
+                                      style: TextStyle(color: Colors.white),
                                     ),
                                     SizedBox(
-                                      width: 22,
+                                      width: 10,
                                     ),
-                                    ClipRRect(
-                                      borderRadius: BorderRadius.all(
-                                        Radius.circular(2),
-                                      ),
-                                      child: Image.asset(
-                                        "assets/user-hero.png",
-                                        height: 22,
-                                        width: 22,
-                                        fit: BoxFit.cover,
+                                    Padding(
+                                      padding: const EdgeInsets.only(top: 4),
+                                      child: SvgPicture.asset(
+                                        'assets/icons/arrow-down.svg',
+                                        height: 4,
+                                        width: 4,
+                                        allowDrawingOutsideViewBox: true,
                                       ),
                                     ),
                                   ],
@@ -121,54 +158,15 @@ class __CustomAppBarMobileState extends State<_CustomAppBarMobile> {
                               ],
                             ),
                           ),
-                        ),
-                        SizedBox(
-                          height: 10,
-                        ),
-                        Container(
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceAround,
-                            children: [
-                              Text(
-                                'Séries',
-                                style: TextStyle(color: Colors.white),
-                              ),
-                              Text(
-                                'Filmes',
-                                style: TextStyle(color: Colors.white),
-                              ),
-                              Row(
-                                crossAxisAlignment: CrossAxisAlignment.center,
-                                children: [
-                                  Text(
-                                    'Categorias',
-                                    style: TextStyle(color: Colors.white),
-                                  ),
-                                  SizedBox(
-                                    width: 10,
-                                  ),
-                                  Padding(
-                                    padding: const EdgeInsets.only(top: 4),
-                                    child: SvgPicture.asset(
-                                      'assets/icons/arrow-down.svg',
-                                      height: 4,
-                                      width: 4,
-                                      allowDrawingOutsideViewBox: true,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
