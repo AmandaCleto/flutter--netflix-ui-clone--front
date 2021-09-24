f50718c3fac20ca70a687fa32a31da5f9f85fc63
AmandaCleto <amanda.clet@gmail.com>
2021-09-23 21:48:21 -0300

    created style for appbar

--- lib/src/pages/home/components/appBar.dart
+++ lib/src/pages/home/components/appBar.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class CustomAppBar extends StatelessWidget {
   final double scrollOffset;
@@ -11,10 +12,6 @@ class CustomAppBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.symmetric(
-        vertical: 10.0,
-        horizontal: 24.0,
-      ),
       color:
           Colors.black.withOpacity((scrollOffset / 350).clamp(0, 1).toDouble()),
       child: _CustomAppBarMobile(),
@@ -67,26 +64,108 @@ class __CustomAppBarMobileState extends State<_CustomAppBarMobile> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Column(
-        children: <Widget>[
-          AnimatedContainer(
-            height: _showAppbar ? 56.0 : 0.0,
-            duration: Duration(milliseconds: 200),
-            child: AppBar(
-              title: Text('Scroll Demo'),
-              actions: <Widget>[
-                //add buttons here
-              ],
-            ),
-          ),
+      child: Flex(
+        direction: Axis.vertical,
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
           Expanded(
-            child: SingleChildScrollView(
-              controller: _scrollViewController,
-              child: Column(
-                children: <Widget>[
-                  //add your screen content here
-                ],
-              ),
+            child: Column(
+              children: <Widget>[
+                AnimatedContainer(
+                  height: _showAppbar ? 100.0 : 0.0,
+                  width: MediaQuery.of(context).size.width,
+                  duration: Duration(milliseconds: 200),
+                  child: AppBar(
+                    backgroundColor: Colors.transparent,
+                    elevation: 0,
+                    flexibleSpace: Column(
+                      children: [
+                        Container(
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Row(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Image.asset(
+                                  "assets/netflix-logo.png",
+                                  width: 20,
+                                  fit: BoxFit.cover,
+                                ),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: [
+                                    SvgPicture.asset(
+                                      'assets/icons/search.svg',
+                                      height: 20,
+                                      width: 20,
+                                      allowDrawingOutsideViewBox: true,
+                                    ),
+                                    SizedBox(
+                                      width: 22,
+                                    ),
+                                    ClipRRect(
+                                      borderRadius: BorderRadius.all(
+                                        Radius.circular(2),
+                                      ),
+                                      child: Image.asset(
+                                        "assets/user-hero.png",
+                                        height: 22,
+                                        width: 22,
+                                        fit: BoxFit.cover,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Container(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              Text(
+                                'Séries',
+                                style: TextStyle(color: Colors.white),
+                              ),
+                              Text(
+                                'Filmes',
+                                style: TextStyle(color: Colors.white),
+                              ),
+                              Row(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    'Categorias',
+                                    style: TextStyle(color: Colors.white),
+                                  ),
+                                  SizedBox(
+                                    width: 10,
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.only(top: 4),
+                                    child: SvgPicture.asset(
+                                      'assets/icons/arrow-down.svg',
+                                      height: 4,
+                                      width: 4,
+                                      allowDrawingOutsideViewBox: true,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
         ],
--- lib/src/pages/home/home.dart
+++ lib/src/pages/home/home.dart
@@ -37,191 +37,62 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       extendBodyBehindAppBar: true,
       appBar: PreferredSize(
-        preferredSize: Size(MediaQuery.of(context).size.width, 100.0),
-        child: CustomAppBar(scrollOffset: 20),
+        preferredSize: Size.fromHeight(100.0),
+        child: CustomAppBar(scrollOffset: 100),
       ),
       body: SingleChildScrollView(
-        child: Expanded(
-          child: Container(
-              height: MediaQuery.of(context).size.height,
-              width: MediaQuery.of(context).size.width,
-              child: Column(
-                children: [
-                  Container(
-                    // color: Colors.red,
-                    height: 600,
-                    decoration: BoxDecoration(
-                      color: Colors.transparent,
-                      image: DecorationImage(
-                        fit: BoxFit.contain,
-                        image: AssetImage(
-                          'assets/home.jpg',
-                        ),
+        child: Flex(direction: Axis.horizontal, children: [
+          Expanded(
+            child: Column(
+              children: [
+                Container(
+                  height: 600,
+                  decoration: BoxDecoration(
+                    color: Colors.transparent,
+                    image: DecorationImage(
+                      fit: BoxFit.fill,
+                      image: AssetImage(
+                        'assets/home.jpg',
                       ),
                     ),
-                    width: MediaQuery.of(context).size.width,
-                    child: Text(
-                      'lista',
-                      style: TextStyle(color: Colors.amber, fontSize: 50),
-                    ),
                   ),
-                  Container(
-                    height: 100,
-                    color: Colors.red,
-                    width: MediaQuery.of(context).size.width,
-                    child: Text(
-                      'lista',
-                      style: TextStyle(color: Colors.amber, fontSize: 50),
-                    ),
+                  width: MediaQuery.of(context).size.width,
+                  child: Text(
+                    'lista',
+                    style: TextStyle(color: Colors.amber, fontSize: 50),
                   ),
-                ],
-              )
-
-              // CustomScrollView(
-              //   slivers: [
-              //     SliverAppBar(
-              //       toolbarHeight: 40,
-              //       pinned: true,
-              //       floating: true,
-              //       brightness: Brightness.light,
-              //       backgroundColor: Colors.transparent,
-              //       excludeHeaderSemantics: false,
-              //       flexibleSpace: FlexibleSpaceBar(
-              //         collapseMode: CollapseMode.pin,
-              //         background: Container(
-              //           color: Colors.transparent,
-              //           child: Padding(
-              //             padding: const EdgeInsets.all(8.0),
-              //             child: Row(
-              //               crossAxisAlignment: CrossAxisAlignment.start,
-              //               mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              //               children: [
-              //                 Image.asset(
-              //                   "assets/netflix-logo.png",
-              //                   width: 20,
-              //                   fit: BoxFit.cover,
-              //                 ),
-              //                 Row(
-              //                   mainAxisAlignment: MainAxisAlignment.center,
-              //                   crossAxisAlignment: CrossAxisAlignment.center,
-              //                   children: [
-              //                     SvgPicture.asset(
-              //                       'assets/icons/search.svg',
-              //                       height: 20,
-              //                       width: 20,
-              //                       allowDrawingOutsideViewBox: true,
-              //                     ),
-              //                     SizedBox(
-              //                       width: 22,
-              //                     ),
-              //                     ClipRRect(
-              //                       borderRadius: BorderRadius.all(
-              //                         Radius.circular(2),
-              //                       ),
-              //                       child: Image.asset(
-              //                         "assets/user-hero.png",
-              //                         height: 20,
-              //                         width: 20,
-              //                         fit: BoxFit.cover,
-              //                       ),
-              //                     ),
-              //                   ],
-              //                 ),
-              //               ],
-              //             ),
-              //           ),
-              //         ),
-              //       ),
-              //       bottom: PreferredSize(
-              //         preferredSize: Size.fromHeight(30),
-              //         child: Container(
-              //           height: 30,
-              //           width: MediaQuery.of(context).size.width,
-              //           color: Colors.transparent,
-              //           child: Row(
-              //             mainAxisAlignment: MainAxisAlignment.spaceAround,
-              //             children: [
-              //               Text(
-              //                 'Séries',
-              //                 style: TextStyle(color: Colors.white),
-              //               ),
-              //               Text(
-              //                 'Filmes',
-              //                 style: TextStyle(color: Colors.white),
-              //               ),
-              //               Row(
-              //                 crossAxisAlignment: CrossAxisAlignment.center,
-              //                 children: [
-              //                   Text(
-              //                     'Categorias',
-              //                     style: TextStyle(color: Colors.white),
-              //                   ),
-              //                   SizedBox(
-              //                     width: 10,
-              //                   ),
-              //                   Padding(
-              //                     padding: const EdgeInsets.only(top: 4),
-              //                     child: SvgPicture.asset(
-              //                       'assets/icons/arrow-down.svg',
-              //                       height: 4,
-              //                       width: 4,
-              //                       allowDrawingOutsideViewBox: true,
-              //                     ),
-              //                   ),
-              //                 ],
-              //               ),
-              //             ],
-              //           ),
-              //         ),
-              //       ),
-              //     ),
-              // SliverList(
-              //   delegate: SliverChildListDelegate(
-              //     <Widget>[
-              //       Padding(
-              //         padding: const EdgeInsets.only(top: 0),
-              //         child: Container(
-              //           color: Colors.blueAccent,
-              //           height: 800,
-              //           child: Column(
-              //             children: [
-              //               // Container(
-              //               //   height: 200,
-              //               //   width: MediaQuery.of(context).size.width,
-              //               //   decoration: BoxDecoration(
-              //               //     gradient: LinearGradient(
-              //               //       colors: [Colors.black, Colors.transparent],
-              //               //       begin: Alignment.bottomCenter,
-              //               //       end: Alignment.topCenter,
-              //               //     ),
-              //               //     image: DecorationImage(
-              //               //       fit: BoxFit.fitHeight,
-              //               //       image: NetworkImage(
-              //               //         pathHomeImgPosterPath,
-              //               //       ),
-              //               //     ),
-              //               //   ),
-              //               // ),
-              //               Container(
-              //                 color: Colors.red,
-              //                 width: MediaQuery.of(context).size.width,
-              //                 child: Text(
-              //                   'lista',
-              //                   style:
-              //                       TextStyle(color: Colors.amber, fontSize: 50),
-              //                 ),
-              //               ),
-              //             ],
-              //           ),
-              //         ),
-              //       ),
-              //     ],
-              //   ),
-              // )
-              //   ],
-              // ),
-              ),
-        ),
+                ),
+                Container(
+                  height: 100,
+                  color: Colors.red,
+                  width: MediaQuery.of(context).size.width,
+                  child: Text(
+                    'lista',
+                    style: TextStyle(color: Colors.amber, fontSize: 50),
+                  ),
+                ),
+                Container(
+                  height: 100,
+                  color: Colors.red,
+                  width: MediaQuery.of(context).size.width,
+                  child: Text(
+                    'lista',
+                    style: TextStyle(color: Colors.amber, fontSize: 50),
+                  ),
+                ),
+                Container(
+                  height: 100,
+                  color: Colors.red,
+                  width: MediaQuery.of(context).size.width,
+                  child: Text(
+                    'lista',
+                    style: TextStyle(color: Colors.amber, fontSize: 50),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ]),
       ),
     );
   }
